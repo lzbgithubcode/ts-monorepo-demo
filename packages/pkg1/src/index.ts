ba2206327bc71fec1027e61193a36b0b6eede51b
lzb <liuzibin@timingbar.com>
2022-12-19 14:33:14 +0800

    feat(pkg1): 引入pkg2包

--- packages/pkg1/src/index.ts
+++ packages/pkg1/src/index.ts
@@ -1,7 +1,10 @@
+import sum2 from "@zb/pkg2";
+
 export const sum1 = (num1: number, num2: number) => {
   console.log("我是第pkg1包------", num1, num2);
   const let = "123";
   const name = "孙悟空";
+  sum2();
   console.log(`我的名字-----${name}`);
 
   return num1 + num2;
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,41 +1,39 @@
-
 import path from "path";
-import {fileURLToPath} from 'url';
-import { createRequire } from 'module'
+import { fileURLToPath } from "url";
+import { createRequire } from "module";
 
 // 查找外部模块
-import {nodeResolve} from "@rollup/plugin-node-resolve"; 
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 // 将commonjs转化为es6
 import commonjs from "@rollup/plugin-commonjs";
 // 编译ts文件
 import typescript from "@rollup/plugin-typescript";
 // 显示文件打下
-import sizes from "@atomico/rollup-plugin-sizes"
+import sizes from "@atomico/rollup-plugin-sizes";
 // 删除js的注释空格
-import cleanup from 'rollup-plugin-cleanup';
+import cleanup from "rollup-plugin-cleanup";
 // 删除指定文件
 import clearDir from "rollup-plugin-clear";
 // babel 编译es5
-import { babel } from '@rollup/plugin-babel';
+import { babel } from "@rollup/plugin-babel";
 
-
-if(!process.env.TARGET){
-  throw new Error("命令中必须 输入TARGET 编译目标",);
+if (!process.env.TARGET) {
+  throw new Error("命令中必须 输入TARGET 编译目标");
 }
 const require = createRequire(import.meta.url);
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
-const resolvePath = (dir,p)=> path.resolve(dir, p);
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const resolvePath = (dir, p) => path.resolve(dir, p);
 // 获取项目的packages包
 const packageDir = resolvePath(__dirname, "packages");
 const targetPkgDir = resolvePath(packageDir, process.env.TARGET);
-const targetPkgDist = resolvePath(targetPkgDir, 'dist');
-const targetPkg = require(resolvePath(targetPkgDir, 'package.json'));
+const targetPkgDist = resolvePath(targetPkgDir, "dist");
+const targetPkg = require(resolvePath(targetPkgDir, "package.json"));
 const name = (targetPkg.buildOptions && targetPkg.buildOptions.name) || path.basename(targetPkgDir);
 const targetTsconfigPath = resolvePath(targetPkgDir, "tsconfig.json");
 
 // 各种插件
 const tsPlugin = typescript({
-  tsconfig:resolvePath(__dirname, "tsconfig.json"),
+  tsconfig: resolvePath(__dirname, "tsconfig.json"),
 });
 const commonjsPlugin = commonjs({
   exclude: "node_modules",
@@ -55,41 +53,31 @@ const clearDirPlugin = clearDir({
 
 // babel编译到es5
 const babelPlugin = babel({
-  exclude: 'node_modules/**',
-  babelHelpers: 'bundled'
+  exclude: "node_modules/**",
+  babelHelpers: "bundled",
 });
-
 
 // 基础配置
 const baseConfig = {
-     input: `${targetPkgDir}/src/index.ts`,
-     output:{
-        name: name,
-        sourcemap: false,
-        minifyInternalExports: true,
-     },
-     plugins:[
-      sizesPlugin,
-      cleanupPlugin,
-      tsPlugin,
-      commonjsPlugin,
-      nodeResolvePlugin,
-      clearDirPlugin,
-      babelPlugin,
-     ]
+  input: `${targetPkgDir}/src/index.ts`,
+  output: {
+    name: name,
+    sourcemap: false,
+    minifyInternalExports: true,
+  },
+  plugins: [sizesPlugin, cleanupPlugin, tsPlugin, commonjsPlugin, nodeResolvePlugin, clearDirPlugin, babelPlugin],
 };
 
 // 编译commonjs， 适用于node与webpack
 const cjsConfig = {
   ...baseConfig,
-  output:{
+  output: {
     format: "cjs",
     file: `${targetPkgDist}/${name}.cjs.js`,
-    ...baseConfig.output
+    ...baseConfig.output,
   },
   plugins: [...baseConfig.plugins],
-}
-
+};
 
 // 编译amd 异步模块定义，用于像RequireJS这样的模块加载器
 const amdConfig = {
@@ -100,7 +88,7 @@ const amdConfig = {
     ...baseConfig.output,
   },
   plugins: [...baseConfig.plugins],
-}
+};
 
 // 编译iife 适合作为<script>标签
 const iifeConfig = {
@@ -111,7 +99,7 @@ const iifeConfig = {
     ...baseConfig.output,
   },
   plugins: [...baseConfig.plugins],
-}
+};
 
 // 编译umd 通用模块定义，以amd，cjs 和 iife 为一体
 const umdConfig = {
@@ -122,8 +110,7 @@ const umdConfig = {
     ...baseConfig.output,
   },
   plugins: [...baseConfig.plugins],
-}
-
+};
 
 // 编译esm模块，适用于<script type=module> 标签引入 或者import
 const esmConfig = {
@@ -143,4 +130,4 @@ const config = {
   umdConfig,
   esmConfig,
 };
-export default  [...Object.values(config)];
+export default [...Object.values(config)];
